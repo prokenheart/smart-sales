f81dd5afc586cdf9a885573a3c4a5fb24c1fd534
prokenheart <2003nhphuc@gmail.com>
2026-01-27 17:32:36 +0700

    add pytest.fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,12 @@
 import sys
 from pathlib import Path
+import pytest
+from unittest.mock import MagicMock
+from sqlalchemy.orm import Session
+
 ROOT_DIR = Path(__file__).resolve().parents[1]
 sys.path.append(str(ROOT_DIR))
+
+@pytest.fixture
+def mock_session():
+    return MagicMock(spec=Session)
