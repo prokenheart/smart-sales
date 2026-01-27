8ddd3fc15c28a9fed0de39f3c06f33d9e4b5949e
prokenheart <2003nhphuc@gmail.com>
2026-01-27 09:23:28 +0700

    add test to status test

--- tests/schemas/test_status.py
+++ tests/schemas/test_status.py
@@ -21,7 +21,28 @@ def test_status_base_upper():
     assert status.status_name == "Unpaid"
     assert status.status_code == "UNPAID"
 
-#Thiếu status_name
+@pytest.mark.parametrize(
+    "status_code",
+    [
+        "",
+        "unpaid2",
+        "unpaid@"
+    ]
+)
+# Sai kiểu status_code
+def test_status_base_invalid_code(status_code):
+    with pytest.raises(ValidationError) as exc_info:
+        StatusBase(
+            status_name="Unpaid",
+            status_code=status_code
+        )
+    error = exc_info.value.errors()[0]
+
+    assert error["loc"] == ("status_code",)
+    assert "only uppercase letters" in error["msg"].lower()
+
+
+# Thiếu status_name
 def test_status_base_missing_status_name():
     with pytest.raises(ValidationError) as exc_info:
         StatusBase(
@@ -32,7 +53,7 @@ def test_status_base_missing_status_name():
     assert error["loc"] == ("status_name",)
     assert error["type"] == "missing"
 
-#Thiếu status_code
+# Thiếu status_code
 def test_status_base_missing_status_code():
     with pytest.raises(ValidationError) as exc_info:
         StatusBase(
