66a81fcafc5f369b6890da8b3e9dfb901b44a4d2
prokenheart <2003nhphuc@gmail.com>
2026-02-09 15:43:03 +0700

    feat: add metrics, tracing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,9 @@
 from app.core.logger import logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools.metrics import Metrics
+from aws_lambda_powertools.metrics import MetricUnit
+from aws_lambda_powertools import Tracer
+
 
 from app.routes.customer import router as customer_router
 from app.routes.product import router as product_router
@@ -9,7 +13,11 @@ from app.routes.user import router as user_router
 from app.routes.order import router as order_router
 from app.routes.item import router as item_router
 
+SERVICE_NAME = "SmartSalesApi"
+
 app = APIGatewayRestResolver(debug=True)
+tracer = Tracer(service=SERVICE_NAME)
+metrics = Metrics(namespace="MyApplication", service=SERVICE_NAME)
 
 # include routers
 app.include_router(customer_router)
@@ -22,11 +30,10 @@ app.include_router(item_router)
 
 
 @logger.inject_lambda_context
+@tracer.capture_lambda_handler
+@metrics.log_metrics(capture_cold_start_metric=True)
 def lambda_handler(event, context):
-    # logger.info("DEBUG_EVENT_PATH", extra={
-    #     "path": event.get("path"),
-    #     "rawPath": event.get("rawPath"),
-    #     "resource": event.get("resource"),
-    #     "requestContext": event.get("requestContext"),
-    # })
+    metrics.add_dimension(name="Path", value="/orders")
+    metrics.add_metric(name="ApiRequest", unit=MetricUnit.Count, value=1)
+
     return app.resolve(event, context)
